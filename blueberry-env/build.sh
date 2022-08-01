ca05193755247c3082f49cc70744ae24779462e6
xmuyoo <xmuyoo@163.com>
2022-08-01 14:04:54 +0800

    Remove outdate blueberry-manager and blueberry-env module

--- blueberry-env/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-WORK_HOME=`pwd`
-
-cd ${WORK_HOME}
-
-docker-compose build
-echo -e "\033[32mAll modules are built successfully!\033[0m"
--- blueberry-env/pulsar/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp ${BLUEBERRY_PULSAR_HOME}/conf/* ${PULSAR_HOME}/conf/
-
-${PULSAR_HOME}/bin/pulsar standalone
